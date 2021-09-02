a13761691225c9556d7520c7210a8c8b527f1f80
voriana <49135736+voriana@users.noreply.github.com>
2021-09-01 22:59:25 -0300

    Metodos
    Creación de métodos en clases:
       FichaMascota.cs
       EsquemaVacunacion.cs
       RegistroServicio.cs

--- Veterinaria/Veterinaria.BibliotecaClases/Entidades/RegistroServicio.cs
+++ Veterinaria/Veterinaria.BibliotecaClases/Entidades/RegistroServicio.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace Veterinaria.BibliotecaClases.Entidades
 {
-    public class PagoConsulta
+    public class RegistroServicio
     {
         //atributo de clase
         private DateTime _fecha;
-        private string _descripcion;
+        private string _tipoServicio;
         private float _monto;
+        private int _clienteId;
+        private int _mascotaId;
     }
 }
